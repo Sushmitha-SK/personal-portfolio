df08de9e3b1ddcdc870cc3524c78ecdb4c24757a
Sushmitha S <94210498+Sushmitha-SK@users.noreply.github.com>
2025-03-17 12:40:31 +0530

    Updated Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <>
             <header id="header" className="header">
                 <nav className="nav container">
-                    <a href="" className="nav__brand">Sushmitha.</a>
-                    <div className="nav__menu" id="nav-menu">
+                    <a href="#home" className="nav__brand">Sushmitha.</a>
+                    <div
+                        className={`nav__menu ${isMenuOpen ? 'nav__menu--open' : ''}`}
+                        id="nav-menu"
+                    >
                         <ul className="nav__list">
                             <li className="nav__item">
                                 <a href="#home" className="nav__link">
@@ -23,31 +32,41 @@ const Header = () => {
                             <li className="nav__item">
                                 <a href="#skills" className="nav__link">
                                     <i className="ri-medal-line nav__icon" />
-                                    Skills</a>
+                                    Skills
+                                </a>
                             </li>
                             <li className="nav__item">
                                 <a href="#education" className="nav__link">
                                     <i className="ri-briefcase-line nav__icon" />
-                                    Qualifications</a>
+                                    Qualifications
+                                </a>
                             </li>
                             <li className="nav__item">
                                 <a href="#portfolio" className="nav__link">
                                     <i className="ri-folders-line nav__icon" />
-                                    Portfolio</a>
+                                    Portfolio
+                                </a>
                             </li>
-
                             <li className="nav__item">
                                 <a href="#contact" className="nav__link">
                                     <i className="ri-phone-line nav__icon" />
-                                    Contact</a>
+                                    Contact
+                                </a>
                             </li>
                         </ul>
+                    </div>
+                    <div
+                        className="nav__toggle"
+                        onClick={toggleMenu}
+                    >
+                        <i className={isMenuOpen ? 'ri-close-line' : 'ri-menu-line'}></i>
                     </div>
                 </nav>
             </header>
 
-        </>
-    )
-}
 
-export default Header
+        </>
+    );
+};
+
+export default Header;
